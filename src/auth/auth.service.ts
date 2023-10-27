ab49194f39e23843980ca23a3b6f6b3857f218b6
wpghks <wpghks56@naver.com>
2023-10-27 10:17:15 +0900

    [feature] password encrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDto } from './dto/request/login.dto';
 import { AuthResponse } from './dto/response/auth.response';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -12,10 +13,13 @@ export class AuthService {
     async login(loginDto: LoginDto): Promise<AuthResponse> {
         const user = await this.prisma.user.findUniqueOrThrow({
             where: {
-                email: loginDto.email,
-                password: loginDto.password
+                email: loginDto.email
             }
         });
+
+        const isPasswordValid = await bcrypt.compare(loginDto.password, user.password);
+
+        if(!isPasswordValid) throw new UnauthorizedException('Invalid Password');
 
         return {
             accessToken: this.jwtService.sign({
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,23 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 import { UserEntity } from './entities/user.entity';
+
+export const roundsOfHashing = 10;
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService){}
 
   async create(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const hashedPassword = await bcrypt.hash(
+      createUserDto.password,
+      roundsOfHashing
+    );
+
+    createUserDto.password = hashedPassword;
+
     return new UserEntity(await this.prisma.user.create({
       data: createUserDto
     }));
@@ -28,6 +38,13 @@ export class UsersService {
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(
+        updateUserDto.password,
+        roundsOfHashing,
+      );
+    }
+
     return new UserEntity(await this.prisma.user.update({
       where: {
         id
